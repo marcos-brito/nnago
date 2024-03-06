03f18974150eebad26c1e1dc9a8ed7ef0217221c
Marcos Brito <marcos.brito0507@gmail.com>
2024-03-06 01:37:01 -0300

    ci: add stuff to deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
-        syntaxHighlight: "prism",
-    },
+  markdown: {
+    syntaxHighlight: "prism",
+  },
+  site: "https://marcos-brito.github.io",
+  base: "/nnago",
 });

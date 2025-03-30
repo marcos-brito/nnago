ba0999ae2f748af2aa86437da4b45f059eb38c44
Marcos Brito <marcos.brito0507@gmail.com>
2025-03-30 20:46:39 -0300

    feat: create a site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    syntaxHighlight: "prism",
-  },
-  site: "https://marcos-brito.github.io",
-  base: "/nnago",
+    markdown: {
+        syntaxHighlight: "prism",
+    },
+
+    site: "https://marcos-brito.github.io/nnago",
+    base: "/nnago",
+    integrations: [sitemap()],
 });

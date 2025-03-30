a34a52dc512626dfcaec8872c3c02278e76709f6
Marcos Brito <marcos.brito0507@gmail.com>
2025-03-30 01:08:39 -0300

    refactor: use meta.glob to get posts

---
+++ src/utils.ts
@@ -0,0 +1,13 @@
+export type FrontMatter = {
+    title: string;
+    image: string;
+    date: string;
+    tags: string;
+    description: string;
+};
+
+const AVERAGE_WORD_PER_MINUTE = 240;
+
+export function calculateReadingTime(text: string): number {
+    return Math.ceil(text.split(" ").length / AVERAGE_WORD_PER_MINUTE);
+}

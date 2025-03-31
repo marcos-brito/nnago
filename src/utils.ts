b7da689a9ac743a1547eca973d64a1dfdf16dbc9
Marcos Brito <marcos.brito0507@gmail.com>
2025-03-31 00:16:40 -0300

    feat: do some SEO

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,16 @@
 export type FrontMatter = {
-  title: string;
-  image: string;
-  date: string;
-  tags: string;
-  description: string;
+    title: string;
+    ogTitle: string;
+    image: string;
+    imageAlt: string;
+    publishedDate: string;
+    modifiedDate: string;
+    tags: string;
+    description: string;
 };
 
 const AVERAGE_WORD_PER_MINUTE = 240;
 
 export function calculateReadingTime(text: string): number {
-  return Math.ceil(text.split(" ").length / AVERAGE_WORD_PER_MINUTE);
+    return Math.ceil(text.split(" ").length / AVERAGE_WORD_PER_MINUTE);
 }
